0360282cab75e948c9e87f5a45128154312797f8
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-02-06 17:47:36 -0300

    User service ok

--- user/src/main/java/br/com/vitorgabrielti/ms/user/services/UserService.java
+++ user/src/main/java/br/com/vitorgabrielti/ms/user/services/UserService.java
@@ -4,19 +4,25 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.vitorgabrielti.ms.user.models.UserModel;
+import br.com.vitorgabrielti.ms.user.producers.UserProducer;
 import br.com.vitorgabrielti.ms.user.repositories.UserRepository;
 
 @Service
 public class UserService {
 
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
+
 
     @Transactional
     public UserModel save(UserModel userModel){
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
     }
 }
