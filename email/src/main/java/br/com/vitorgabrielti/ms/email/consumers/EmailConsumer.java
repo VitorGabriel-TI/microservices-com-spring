65152e908119589d5d22aee7cb00998b20791ffd
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2024-02-07 03:10:42 -0300

    Projeto concluído

--- email/src/main/java/br/com/vitorgabrielti/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/br/com/vitorgabrielti/ms/email/consumers/EmailConsumer.java
@@ -1,15 +1,27 @@
 package br.com.vitorgabrielti.ms.email.consumers;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import br.com.vitorgabrielti.ms.email.dtos.EmailRecordDto;
+import br.com.vitorgabrielti.ms.email.models.EmailModel;
+import br.com.vitorgabrielti.ms.email.services.EmailService;
 
 @Component
 public class EmailConsumer {
+
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
-        System.out.println(emailRecordDto.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
